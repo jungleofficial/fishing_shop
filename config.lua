a587ca8c991f5686b1b8ea7c73eac5e049c08ed2
Jungle_NPC <fhcjcjckfjjfjc@gmail.com>
2024-12-25 14:21:36 +0100

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,8 @@
+Config = {}
+
+Config.FishingShopCoords = vector3(1234.56, -1234.56, 34.56) -- Remplacez par vos coordonnées
+Config.FishingItems = {
+    {label = "Canne à pêche", price = 100},
+    {label = "Hameçons", price = 20},
+    {label = "Appâts", price = 10}
+}
