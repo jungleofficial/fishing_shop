e7354d54d0a8fcd369748cbf0288d5eecbb3a628
Jungle_NPC <fhcjcjckfjjfjc@gmail.com>
2024-12-25 14:20:59 +0100

    Create fxmanifest.lua

---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+author 'VotreNom'
+description 'Magasin de Pêche pour ESX et QBcore'
+version '1.0'
+
+client_scripts {
+    'client.lua',
+}
+
+server_scripts {
+    'server.lua',
+}
+
+shared_script 'config.lua'
